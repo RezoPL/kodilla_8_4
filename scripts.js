6bc50adbd0cfeaf8d151c210e82e8b64a2b14c1d
Przemek Jurkiewicz <hankusekbb@gmail.com>
2017-12-12 20:31:21 +0100

    add comments to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 // scripts.js
-
+// funkcja z pytaniem o imię oraz polem do wpisania wartości
 var name = prompt('Enter your name');
+// wywołanie funkcji alert z parametrem
 alert('Hello, ' + name);
+/* wywołanie funkcji z parametrem name i wyświetlenie na zakładce konsola */
 console.log('Hello, ' + name);
